d96ba7454765344d7d64115844a083cf9b8d907a
gtanman <65194596+gtanman@users.noreply.github.com>
2024-09-05 22:54:29 +0200

    Update Celery configuration to use Redis

--- celery_factory.py
+++ celery_factory.py
@@ -5,21 +5,11 @@ def make_celery(app):
     # Get the Redis URL from the environment variable
     redis_url = os.getenv('REDIS_URL')
 
-    # Create the Celery application with the Flask app's name
-    celery = Celery(app.import_name, broker=redis_url)
+    # Create the Celery application with Redis as the broker and result backend
+    celery = Celery(app.import_name, broker=redis_url, backend=redis_url)
 
     # Update Celery config from the Flask app's config
     celery.conf.update(app.config)
-
-    # Set result backend and other Celery configurations
-    celery.conf.update(
-        result_backend=redis_url,
-        task_serializer='json',
-        accept_content=['json'],
-        result_serializer='json',
-        timezone='UTC',
-        enable_utc=True,
-    )
 
     class ContextTask(celery.Task):
         def __call__(self, *args, **kwargs):
