465b3f546fa1f1f0ecea9d99e39073d3f430782e
gtanman <65194596+gtanman@users.noreply.github.com>
2024-09-05 22:40:25 +0200

    Set up Redis with SSL for Celery

--- celery_factory.py
+++ celery_factory.py
@@ -2,8 +2,8 @@ from celery import Celery
 import os
 
 def make_celery(app):
-    # Get the Redis URL from the environment variable set by Heroku
-    redis_url = os.getenv('REDIS_URL')
+    # Get the Redis URL from the environment variable or use the one provided directly
+    redis_url = os.getenv('REDIS_URL', 'rediss://:your_password@your_host:8710')
 
     # Create the Celery application with the Flask app's name
     celery = Celery(app.import_name, broker=redis_url)
@@ -13,6 +13,7 @@ def make_celery(app):
 
     # Set result backend and other Celery configurations
     celery.conf.update(
+        broker_use_ssl={'ssl_cert_reqs': 'CERT_NONE'},  # To support SSL Redis
         result_backend=redis_url,
         task_serializer='json',
         accept_content=['json'],
